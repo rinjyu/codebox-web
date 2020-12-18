31ed4a786bdc8399a05228ce4c07ac92002f8002
rinjyu <rinjyu@naver.com>
2020-12-18 18:59:41 +0900

    [feat]: codebox.web 생성
    codebox.web 생성

---
+++ src/main/java/com/app/codebox/web/CodeBoxWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.codebox.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBoxWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBoxWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/codebox/web/CodeBoxWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.codebox.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBoxWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
